e7c2d9aa2373bb4df29f503230c70c8066afadf5
Роман Недосеко <freelancerroma@mail.ru>
2025-08-13 14:49:28 +0300

    usermod

---
+++ Final_Project_ML/schema.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+
+from pydantic import BaseModel
+
+
+class UserGet(BaseModel):
+    id: int
+    gender: int
+    age: int
+    country: str
+    city: str
+    exp_group: int
+    os: str
+    source: str
+
+    class Config:
+        from_attributes = True
+
+
+class PostGet(BaseModel):
+    
