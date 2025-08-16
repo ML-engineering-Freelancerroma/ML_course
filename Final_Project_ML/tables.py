646f372c422cbbffa658236a3dd6639ecd6cacd0
Роман Недосеко <freelancerroma@mail.ru>
2025-08-16 12:45:39 +0300

    tablefeed2

--- Final_Project_ML/tables.py
+++ Final_Project_ML/tables.py
@@ -6,9 +6,9 @@ from database import Base
 
 class Feed(Base):
     __tablename__ = 'feed_action'
-    user_id =
-    user =
-    post_id =
-    post = 
-    action = 
+    user_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
+    user = relationship(User)
+    post_id = Column(Integer, ForeignKey('post.id'), primary_key=True)
+    post = relationship(Post)
+    action = Column(String)
     time = Column(TIMESTAMP)
