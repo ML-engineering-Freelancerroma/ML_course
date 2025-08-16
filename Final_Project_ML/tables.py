9ccfc0a49e9827a46ac0b0497b908cedaa345b51
Роман Недосеко <freelancerroma@mail.ru>
2025-08-17 00:23:02 +0300

    post user notav

--- Final_Project_ML/tables.py
+++ Final_Project_ML/tables.py
@@ -4,6 +4,23 @@ from sqlalchemy.orm import relationship
 from database import Base
 
 
+class Post(Base):
+    pass
+
+
+class User(Base):
+    __tablename__ = 'user'
+    id = 
+    gender = 
+    age = 
+    country = 
+    city = 
+    exp_group = 
+    os = 
+    source = 
+    pass
+
+
 class Feed(Base):
     __tablename__ = 'feed_action'
     user_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
