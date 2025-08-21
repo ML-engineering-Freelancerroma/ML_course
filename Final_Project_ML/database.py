f259f759a8287703b4d784f05f3e5212d4653b00
Роман Недосеко <freelancerroma@mail.ru>
2025-08-21 15:05:23 +0300

    via dotenv

--- Final_Project_ML/database.py
+++ Final_Project_ML/database.py
@@ -1,9 +1,13 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-SQLALCHEMY_DATABASE_URL = 
+load_dotenv()
+
+SQLALCHEMY_DATABASE_URL = os.getenv(
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
