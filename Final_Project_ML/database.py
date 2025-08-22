414af9390e1d295dabf7b1bfc32682d1189a5421
Роман Недосеко <freelancerroma@mail.ru>
2025-08-22 12:21:15 +0300

    dotenv

--- Final_Project_ML/database.py
+++ Final_Project_ML/database.py
@@ -3,11 +3,11 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-
+from dotenv import load_dotenv
 
 load_dotenv()
 
-SQLALCHEMY_DATABASE_URL = os.getenv(
+SQLALCHEMY_DATABASE_URL = os.getenv('DATABASE')  # postgresql://robot-startml-ro:pheiph0hahj1Vaif@postgres.lab.karpov.courses:6432/startml
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
