7351341afb89868fbddbe77bd042848e55e3d75d
Роман Недосеко <freelancerroma@mail.ru>
2025-08-23 13:48:00 +0300

    some fix

--- Final_Project_ML/app.py
+++ Final_Project_ML/app.py
@@ -4,10 +4,8 @@ from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 from sqlalchemy import desc, func
 
-# from schema import UserGet, PostGet, FeedGet
-# from table_post import Post
-# from table_user import User
-# from table_feed import Feed
+from schema import UserGet, PostGet, FeedGet
+from tables import Post, User, Feed
 from database import SessionLocal
 
 
@@ -21,7 +19,11 @@ def get_db():
 
 @app.get('/user/{id}', response_model=UserGet)
 def user_get(id: int, db: Session = Depends(get_db)):
-    response = db.query(User).filter(User.id == id).one_or_none()
+    response = (
+        db.query(User).
+        filter(User.id == id).
+        one_or_none()
+    )
     if response is None:
         raise HTTPException(404, f'No user with id {id}')
     return response
@@ -29,7 +31,11 @@ def user_get(id: int, db: Session = Depends(get_db)):
 
 @app.get('/post/{id}', response_model=PostGet)
 def post_get(id: int, db: Session = Depends(get_db)):
-    response = db.query(Post).filter(Post.id == id).one_or_none()
+    response = (
+        db.query(Post).
+        filter(Post.id == id).
+        one_or_none()
+    )
     if response is None:
         raise HTTPException(404, f'No post with id {id}')
     return response
@@ -37,26 +43,32 @@ def post_get(id: int, db: Session = Depends(get_db)):
 
 @app.get('/user/{id}/feed', response_model=List[FeedGet])
 def feed_user_get(id: int, limit=10, db: Session = Depends(get_db)):
-    response = db.query(Feed).filter(Feed.user_id == id).order_by(desc(Feed.time)).limit(limit).all()
-    return response
-
-
-@app.get('/post/{id}/feed', response_model=List[FeedGet])
-def feed_user_get(id: int, limit=10, db: Session = Depends(get_db)):
-    response = db.query(Feed).filter(Feed.post_id == id).order_by(desc(Feed.time)).limit(limit).all()
-    return response
-
-
-@app.get('/post/recommendations/', response_model=List[PostGet])
-def get_recommended_feed(id: int, limit=10, db: Session = Depends(get_db)) -> List[PostGet]:
     response = (
-        db.query(Post.id, Post.text, Post.topic).
-        select_from(Feed).
-        join(Post, Post.id == Feed.post_id).
-        filter(Feed.action == 'like').
-        group_by(Post.id).
-        order_by(func.count(Feed.post_id).desc()).
+        db.query(Feed).
+        filter(Feed.user_id == id).
+        order_by(desc(Feed.time)).
         limit(limit).
         all()
     )
     return response
+
+
+# @app.get('/post/{id}/feed', response_model=List[FeedGet])
+# def feed_user_get(id: int, limit=10, db: Session = Depends(get_db)):
+#     response = db.query(Feed).filter(Feed.post_id == id).order_by(desc(Feed.time)).limit(limit).all()
+#     return response
+
+
+# @app.get('/post/recommendations/', response_model=List[PostGet])
+# def get_recommended_feed(id: int, limit=10, db: Session = Depends(get_db)) -> List[PostGet]:
+#     response = (
+#         db.query(Post.id, Post.text, Post.topic).
+#         select_from(Feed).
+#         join(Post, Post.id == Feed.post_id).
+#         filter(Feed.action == 'like').
+#         group_by(Post.id).
+#         order_by(func.count(Feed.post_id).desc()).
+#         limit(limit).
+#         all()
+#     )
+#     return response
