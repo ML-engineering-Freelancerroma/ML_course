994f11b79cc21e1a1a05fd8345139df9a8c11991
Роман Недосеко <freelancerroma@mail.ru>
2025-09-01 10:43:05 +0300

    load feat

--- Final_Project_ML/service.py
+++ Final_Project_ML/service.py
@@ -1,3 +1,5 @@
+import os
+
 import pandas as pd
 from sqlalchemy import create_engine
 from dotenv import load_dotenv
@@ -5,13 +7,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+# Database =
+# postgresql://robot-startml-ro:pheiph0hahj1Vaif@postgres.lab.karpov.courses:6432/startml
+
 
 def batch_load_sql(query: str) -> pd.DataFrame:
     CHUNKSIZE = 200000
-    engine = create_engine(
-        # "postgresql://robot-startml-ro:pheiph0hahj1Vaif@"
-        # "postgres.lab.karpov.courses:6432/startml"
-    )
+    engine = create_engine(os.getenv('DATABASE'))
     conn = engine.connect().execution_options(stream_results=True)
     chunks = []
     for chunk_dataframe in pd.read_sql(query, conn, chunksize=CHUNKSIZE):
