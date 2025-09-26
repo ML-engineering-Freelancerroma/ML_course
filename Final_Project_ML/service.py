9c5e397e83ec06c1e0d871b029212910be995468
Роман Недосеко <freelancerroma@mail.ru>
2025-09-26 13:51:59 +0300

    some add

--- Final_Project_ML/service.py
+++ Final_Project_ML/service.py
@@ -105,7 +105,8 @@ def load_post_texts(post_ids: List[int]) -> List[dict]:
 
 
 def get_exp_group(user_id: int) -> str:
-    res = int(
-        hashlib.md5((str(user_id) + 'my_unique_salt').encode()).hexdigest(), 16
-    ) % 2
-    return str(res)
+    salt = os.getenv('SALT')
+    # result = int(
+    #     hashlib.sha256((str(user_id) + 'salt').encode()).hexdigest(), 16
+    # ) % 2
+    return str(result)
